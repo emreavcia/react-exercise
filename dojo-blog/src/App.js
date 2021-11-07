e5dea803c44389ec1cc78e6a454882b328efd077
emreavcia <avciemrea@gmail.com>
2021-11-07 22:48:24 +0100

    react links are put

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home/>}>
-              
+            <Route exact path="/" element={<Home/>}> 
+            </Route>
+            <Route path="/create" element={<Create/>}> 
             </Route>
           </Routes>
         </div>
---
+++ dojo-blog/src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return ( 
+        <div className="create">
+            <div>Add a New Blog</div>
+        </div>
+     );
+}
+ 
+export default Create;
